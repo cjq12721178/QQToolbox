8f5d070d77d1a16f967c9a5ba16c483768cc5836
cjq <cjq08122108@sina.cn>
2017-09-01 09:19:43 +0800

    20170901-weisi-1.1.0(3): 1. 增加传感器添加监听接口

--- weisi/src/main/java/com/cjq/lib/weisi/sensor/SensorManager.java
+++ weisi/src/main/java/com/cjq/lib/weisi/sensor/SensorManager.java
@@ -1,6 +1,7 @@
 package com.cjq.lib.weisi.sensor;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -10,24 +11,50 @@ import java.util.Map;
 public class SensorManager {
 
     private static final Map<Integer, Sensor> SENSOR_MAP = new HashMap<>();
+    private static OnSensorAddedListener onSensorAddedListener;
 
-    public static Sensor getSensor(int address) {
+    private SensorManager() {
+    }
+
+    public static synchronized Sensor getSensor(int address) {
         Sensor sensor = SENSOR_MAP.get(address);
         if (sensor == null) {
             sensor = new Sensor(address);
-            SENSOR_MAP.put(address, sensor);
+            addSensor(address, sensor);
         }
         return sensor;
     }
 
-    public static Sensor buildSensor(int address, SensorDecorator decorator) {
+    public static synchronized Sensor buildSensor(int address, SensorDecorator decorator) {
         Sensor sensor = SENSOR_MAP.get(address);
         if (sensor == null) {
             sensor = new Sensor(address, decorator);
-            SENSOR_MAP.put(address, sensor);
+            addSensor(address, sensor);
         } else {
             sensor.setDecorator(decorator);
         }
         return sensor;
+    }
+
+    public static synchronized void getAllSensors(List<Sensor> sensorCarrier) {
+        if (sensorCarrier == null) {
+            return;
+        }
+        sensorCarrier.addAll(SENSOR_MAP.values());
+    }
+
+    private static void addSensor(int address, Sensor sensor) {
+        SENSOR_MAP.put(address, sensor);
+        if (onSensorAddedListener != null) {
+            onSensorAddedListener.onSensorAdded(sensor);
+        }
+    }
+
+    public static void setOnSensorAddedListener(OnSensorAddedListener listener) {
+        onSensorAddedListener = listener;
+    }
+
+    public interface OnSensorAddedListener {
+        void onSensorAdded(Sensor sensor);
     }
 }
